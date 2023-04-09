9d47d0df427fddc9d4577d718cc214e144355fe6
Nitesh <shuklanitesh805@gmail.com>
2023-04-09 19:45:45 +0530

    props and useState

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import ListGroup from "./components/ListGroup";
 
 function App(){
     //return <div><Message /></div>;
-    return <div><ListGroup/></div>
+    let items = ['New York', 'Los Angeles', 'Nashvillie', 'Captown'];
+    return <div><ListGroup items={items} heading='List'/></div>
     
 }
 
