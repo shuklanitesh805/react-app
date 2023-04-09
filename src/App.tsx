695d0b03c87b75e4e1c7e4d2ad696493ea857308
Nitesh <shuklanitesh805@gmail.com>
2023-04-09 19:55:16 +0530

    pass value from prop and handle function

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ import ListGroup from "./components/ListGroup";
 function App(){
     //return <div><Message /></div>;
     let items = ['New York', 'Los Angeles', 'Nashvillie', 'Captown'];
-    return <div><ListGroup items={items} heading='List'/></div>
+    const handleSelectItem = (item:string) =>{
+        console.log(item);
+    }
+    return <div><ListGroup items={items} heading='Cities' onSelectItem={handleSelectItem}/></div>
     
 }
 
