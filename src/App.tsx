7fae1a54cf26a3a73752a1456f75b0a3be200f5d
Nitesh <shuklanitesh805@gmail.com>
2023-04-09 16:55:57 +0530

    add key to list item to resolve key error on  browser console

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import Message from "./Message";
+//import Message from "./Message";
 import ListGroup from "./components/ListGroup";
 
 function App(){
     //return <div><Message /></div>;
-    const items = [
-      'New York', 'Los Angeles', 'Nashvillie', 'Captown'
-    ];
-    return (
-    <><h1>LIST</h1><ul className="list-group">
-        {items.map(item => <li className="list-group-item">{item}</li>)}
-      </ul></>);
+    return <div><ListGroup/></div>
+    
 }
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,11 @@
 function ListGroup(){
-    return <h1>List Group</h1>
+    const items = [
+        'New York', 'Los Angeles', 'Nashvillie', 'Captown'
+      ];
+      return (
+      <><h1>LIST</h1><ul className="list-group">
+          {items.map(item => <li className="list-group-item" key={item}>{item}</li>)}
+        </ul></>);
 }
 
 export default ListGroup;
