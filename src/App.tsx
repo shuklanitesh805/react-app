c2d2abf2347394359a79c8979d99d7c558ab9774
Nitesh <shuklanitesh805@gmail.com>
2023-04-09 20:39:00 +0530

    fix merge code issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 //import Message from "./Message";
-
 import Alert from "./components/Alert";
+import ListGroup from "./components/ListGroup";
 
+function App(){
+    //return <div><Message /></div>;
     let items = ['New York', 'Los Angeles', 'Nashvillie', 'Captown'];
     const handleSelectItem = (item:string) =>{
         console.log(item);
@@ -10,6 +12,7 @@ import Alert from "./components/Alert";
     <Alert >
         <span>Hello Workers!!</span>
     </Alert></div></>
+    
 }
 
 export default App;
