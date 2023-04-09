0337cdd47539580549912b28f18ac84847ea99f9
Nitesh shukla <41079916+shuklanitesh805@users.noreply.github.com>
2023-04-09 20:31:47 +0530

    final code push with child component, use state and props (#3)
    * add bootstrap library
    
    * add listgroup
    
    * add fragment with multiple elment return
    
    * add dynamic list item
    
    * add key to list item to resolve key error on  browser console
    
    * add condition to show render item
    
    * props and useState
    
    * extract attribute from prop
    
    * pass value from prop and handle function
    
    * add child component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 //import Message from "./Message";
-import ListGroup from "./components/ListGroup";
 
-function App(){
-    //return <div><Message /></div>;
-    return <div><ListGroup/></div>
-    
+import Alert from "./components/Alert";
+
+    let items = ['New York', 'Los Angeles', 'Nashvillie', 'Captown'];
+    const handleSelectItem = (item:string) =>{
+        console.log(item);
+    }
+    return <><div><ListGroup items={items} heading='Cities' onSelectItem={handleSelectItem}/> 
+    <Alert >
+        <span>Hello Workers!!</span>
+    </Alert></div></>
 }
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,23 @@
-function ListGroup(){
-    let items = [
-        'New York', 'Los Angeles', 'Nashvillie', 'Captown'
-      ];
+import { MouseEventHandler, useState } from "react";
+
+interface PropsListGroup{
+  items: string[];
+  heading: string;
+  //function for event
+  onSelectItem: (item:string) => void;
+}
+
+function ListGroup({items, heading, onSelectItem}: PropsListGroup){
+  
+    //let items = [ 'New York', 'Los Angeles', 'Nashvillie', 'Captown'];
       //items= [];
+      const [selectedIndex, setSelectedIndex] = useState(-1);
+      //const handleSelectedIndex = (item: string): MouseEventHandler => {console.log(item)};
       return (
       <>
       {items.length === 0 ? <p>Not Found</p> : null}
-      <h1>LIST</h1><ul className="list-group">
-          {items.map(item => <li className="list-group-item" key={item}>{item}</li>)}
+      <h1>{heading}</h1><ul className="list-group">
+          {items.map((item, index) => <li className = {selectedIndex === index ? "list-group-item active" : "list-group-item" } key={item} onClick={ () => {setSelectedIndex(index); onSelectItem(item);}}>{item}</li>)}
         </ul></>);
 }
 
